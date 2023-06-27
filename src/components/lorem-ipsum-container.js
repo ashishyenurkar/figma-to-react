43bacb1c17e8db8016a0a75241b4ca43b501d1c1
Ashish Yenurkar <ashishyenurkar15@gmail.com>
2023-06-27 11:13:59 +0530

    figma to react app

---
+++ src/components/lorem-ipsum-container.js
@@ -0,0 +1,77 @@
+import "./lorem-ipsum-container.css";
+const LoremIpsumContainer = () => {
+  return (
+    <div className="vector-parent">
+      <img className="group-child" alt="" src="/vector-5.svg" />
+      <div className="what-is-lorem">What is Lorem Ipsum?</div>
+      <div className="what-is-lorem1">What is Lorem Ipsum?</div>
+      <div className="what-is-lorem2">What is Lorem Ipsum?</div>
+      <div className="what-is-lorem3">What is Lorem Ipsum?</div>
+      <div className="what-is-lorem4">What is Lorem Ipsum?</div>
+      <div className="what-is-lorem5">What is Lorem Ipsum?</div>
+      <div className="lorem-ipsum-is">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </div>
+      <div className="lorem-ipsum-is1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </div>
+      <div className="lorem-ipsum-is2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </div>
+      <div className="lorem-ipsum-is3">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </div>
+      <div className="lorem-ipsum-is4">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </div>
+      <div className="lorem-ipsum-is5">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </div>
+      <img className="group-icon" alt="" src="/group.svg" />
+      <div className="ellipse-parent">
+        <div className="frame-child" />
+        <img className="frame-item" alt="" src="/group-6062.svg" />
+        <b className="b">01</b>
+      </div>
+      <div className="ellipse-group">
+        <div className="frame-child" />
+        <img className="frame-item" alt="" src="/group-6062.svg" />
+        <b className="b">06</b>
+      </div>
+      <div className="ellipse-container">
+        <div className="frame-child" />
+        <img className="frame-item" alt="" src="/group-60621.svg" />
+        <b className="b2">02</b>
+      </div>
+      <div className="frame-div">
+        <div className="frame-child" />
+        <img className="frame-item" alt="" src="/group-60621.svg" />
+        <b className="b2">05</b>
+      </div>
+      <div className="ellipse-parent1">
+        <div className="frame-child" />
+        <img className="frame-item" alt="" src="/group-6062.svg" />
+        <b className="b2">03</b>
+      </div>
+      <div className="ellipse-parent2">
+        <div className="frame-child" />
+        <img className="frame-item" alt="" src="/group-6062.svg" />
+        <b className="b2">04</b>
+      </div>
+    </div>
+  );
+};
+
+export default LoremIpsumContainer;
---
+++ src/pages/website-design.js
@@ -0,0 +1,220 @@
+import LoremIpsumContainer from "../components/lorem-ipsum-container";
+import "./website-design.css";
+const WebsiteDesign = () => {
+  return (
+    <div className="website-design">
+      <div className="section-1">
+        <div className="rectangle" />
+        <img className="line-icon" alt="" src="/line.svg" />
+        <div className="dot-ornament">
+          <div className="rectangle-3-copy-19" />
+          <div className="rectangle-3-copy-18" />
+          <div className="rectangle-3-copy-17" />
+          <div className="rectangle-3-copy-16" />
+          <div className="rectangle-3-copy-15" />
+          <div className="rectangle-3-copy-14" />
+          <div className="rectangle-3-copy-13" />
+          <div className="rectangle-3-copy-12" />
+          <div className="rectangle-3-copy-11" />
+          <div className="rectangle-3-copy-10" />
+          <div className="rectangle-3-copy-29" />
+          <div className="rectangle-3-copy-28" />
+          <div className="rectangle-3-copy-27" />
+          <div className="rectangle-3-copy-26" />
+          <div className="rectangle-3-copy-25" />
+          <div className="rectangle-3-copy-24" />
+          <div className="rectangle-3-copy-23" />
+          <div className="rectangle-3-copy-22" />
+          <div className="rectangle-3-copy-21" />
+          <div className="rectangle-3-copy-20" />
+          <div className="rectangle-3-copy-39" />
+          <div className="rectangle-3-copy-38" />
+          <div className="rectangle-3-copy-37" />
+          <div className="rectangle-3-copy-36" />
+          <div className="rectangle-3-copy-35" />
+          <div className="rectangle-3-copy-34" />
+          <div className="rectangle-3-copy-33" />
+          <div className="rectangle-3-copy-32" />
+          <div className="rectangle-3-copy-31" />
+          <div className="rectangle-3-copy-30" />
+          <div className="rectangle-3-copy-49" />
+          <div className="rectangle-3-copy-48" />
+          <div className="rectangle-3-copy-47" />
+          <div className="rectangle-3-copy-46" />
+          <div className="rectangle-3-copy-45" />
+          <div className="rectangle-3-copy-44" />
+          <div className="rectangle-3-copy-43" />
+          <div className="rectangle-3-copy-42" />
+          <div className="rectangle-3-copy-41" />
+          <div className="rectangle-3-copy-40" />
+          <div className="rectangle-3-copy-59" />
+          <div className="rectangle-3-copy-58" />
+          <div className="rectangle-3-copy-57" />
+          <div className="rectangle-3-copy-56" />
+          <div className="rectangle-3-copy-55" />
+          <div className="rectangle-3-copy-54" />
+          <div className="rectangle-3-copy-53" />
+          <div className="rectangle-3-copy-52" />
+          <div className="rectangle-3-copy-51" />
+          <div className="rectangle-3-copy-50" />
+          <div className="rectangle-3-copy-69" />
+          <div className="rectangle-3-copy-68" />
+          <div className="rectangle-3-copy-67" />
+          <div className="rectangle-3-copy-66" />
+          <div className="rectangle-3-copy-65" />
+          <div className="rectangle-3-copy-64" />
+          <div className="rectangle-3-copy-63" />
+          <div className="rectangle-3-copy-62" />
+          <div className="rectangle-3-copy-61" />
+          <div className="rectangle-3-copy-60" />
+          <div className="rectangle-3-copy-79" />
+          <div className="rectangle-3-copy-78" />
+          <div className="rectangle-3-copy-77" />
+          <div className="rectangle-3-copy-76" />
+          <div className="rectangle-3-copy-75" />
+          <div className="rectangle-3-copy-74" />
+          <div className="rectangle-3-copy-73" />
+          <div className="rectangle-3-copy-72" />
+          <div className="rectangle-3-copy-71" />
+          <div className="rectangle-3-copy-70" />
+        </div>
+        <div className="dot-ornament1">
+          <div className="rectangle-3-copy-19" />
+          <div className="rectangle-3-copy-18" />
+          <div className="rectangle-3-copy-17" />
+          <div className="rectangle-3-copy-16" />
+          <div className="rectangle-3-copy-15" />
+          <div className="rectangle-3-copy-14" />
+          <div className="rectangle-3-copy-13" />
+          <div className="rectangle-3-copy-12" />
+          <div className="rectangle-3-copy-11" />
+          <div className="rectangle-3-copy-10" />
+          <div className="rectangle-3-copy-29" />
+          <div className="rectangle-3-copy-28" />
+          <div className="rectangle-3-copy-27" />
+          <div className="rectangle-3-copy-26" />
+          <div className="rectangle-3-copy-25" />
+          <div className="rectangle-3-copy-24" />
+          <div className="rectangle-3-copy-23" />
+          <div className="rectangle-3-copy-22" />
+          <div className="rectangle-3-copy-21" />
+          <div className="rectangle-3-copy-20" />
+          <div className="rectangle-3-copy-39" />
+          <div className="rectangle-3-copy-38" />
+          <div className="rectangle-3-copy-37" />
+          <div className="rectangle-3-copy-36" />
+          <div className="rectangle-3-copy-35" />
+          <div className="rectangle-3-copy-34" />
+          <div className="rectangle-3-copy-33" />
+          <div className="rectangle-3-copy-32" />
+          <div className="rectangle-3-copy-31" />
+          <div className="rectangle-3-copy-30" />
+          <div className="rectangle-3-copy-49" />
+          <div className="rectangle-3-copy-48" />
+          <div className="rectangle-3-copy-47" />
+          <div className="rectangle-3-copy-46" />
+          <div className="rectangle-3-copy-45" />
+          <div className="rectangle-3-copy-44" />
+          <div className="rectangle-3-copy-43" />
+          <div className="rectangle-3-copy-42" />
+          <div className="rectangle-3-copy-41" />
+          <div className="rectangle-3-copy-40" />
+          <div className="rectangle-3-copy-59" />
+          <div className="rectangle-3-copy-58" />
+          <div className="rectangle-3-copy-57" />
+          <div className="rectangle-3-copy-56" />
+          <div className="rectangle-3-copy-55" />
+          <div className="rectangle-3-copy-54" />
+          <div className="rectangle-3-copy-53" />
+          <div className="rectangle-3-copy-52" />
+          <div className="rectangle-3-copy-51" />
+          <div className="rectangle-3-copy-50" />
+          <div className="rectangle-3-copy-69" />
+          <div className="rectangle-3-copy-68" />
+          <div className="rectangle-3-copy-67" />
+          <div className="rectangle-3-copy-66" />
+          <div className="rectangle-3-copy-65" />
+          <div className="rectangle-3-copy-64" />
+          <div className="rectangle-3-copy-63" />
+          <div className="rectangle-3-copy-62" />
+          <div className="rectangle-3-copy-61" />
+          <div className="rectangle-3-copy-60" />
+          <div className="rectangle-3-copy-79" />
+          <div className="rectangle-3-copy-78" />
+          <div className="rectangle-3-copy-77" />
+          <div className="rectangle-3-copy-76" />
+          <div className="rectangle-3-copy-75" />
+          <div className="rectangle-3-copy-74" />
+          <div className="rectangle-3-copy-73" />
+          <div className="rectangle-3-copy-72" />
+          <div className="rectangle-3-copy-71" />
+          <div className="rectangle-3-copy-70" />
+        </div>
+        <div className="about">
+          <div className="quis-nostrud-exercit-parent">
+            <div className="quis-nostrud-exercit">
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in
+            </div>
+            <div className="about1">ABOUT</div>
+            <img className="path-icon" alt="" src="/path.svg" />
+          </div>
+        </div>
+        <img className="box-icon" alt="" src="/box@2x.png" />
+        <div className="karen-william">
+          <div className="karen-william1">Karen William</div>
+          <div className="senior-designer">Senior Designer</div>
+        </div>
+        <div className="watch-trailer">
+          <div className="karen-william1">Watch Trailer</div>
+          <div className="senior-designer">2 mins</div>
+        </div>
+        <img className="box-icon1" alt="" src="/box1@2x.png" />
+        <div className="testimonial">Testimonial</div>
+        <div className="tom-hope">Tom Hope</div>
+        <div className="div">20:45</div>
+        <div className="group-5-copy">
+          <div className="rectangle-copy-6" />
+          <img className="triangle-icon" alt="" src="/triangle.svg" />
+        </div>
+        <img className="box-icon2" alt="" src="/box2@2x.png" />
+        <div className="box" />
+        <div className="cta">
+          <div className="yellowbox" />
+          <div className="join-the-class-container">
+            <p className="join">Join</p>
+            <p className="join">The Class</p>
+          </div>
+          <img className="arrow-icon" alt="" src="/arrow.svg" />
+        </div>
+        <img className="audio-icon" alt="" src="/audio.svg" />
+        <div className="the-role-of-container">
+          <p className="join">The role of</p>
+          <p className="join">User Interface</p>
+          <p className="join">Designer</p>
+        </div>
+        <div className="featured-label">
+          <div className="featured-class">FEATURED CLASS</div>
+          <img className="path-icon1" alt="" src="/path1.svg" />
+        </div>
+        <div className="menu">
+          <div className="explore">EXPLORE</div>
+          <div className="mentors">MENTORS</div>
+          <div className="blog">BLOG</div>
+          <div className="community">COMMUNITY</div>
+        </div>
+        <div className="logo">
+          <div className="oval" />
+          <b className="c">C</b>
+        </div>
+      </div>
+      <LoremIpsumContainer />
+      <div className="cta1">
+        <div className="yellowbox1" />
+        <div className="join-the-class">Join Us</div>
+      </div>
+    </div>
+  );
+};
+
+export default WebsiteDesign;
